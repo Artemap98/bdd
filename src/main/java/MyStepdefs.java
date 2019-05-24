f30137c48ddc2ad1ca49261fb5e5356837117e89
artem popov <artemap98@gmail.com>
2019-05-24 11:27:58 +0700

    Created first scenario tests

---
+++ src/main/java/MyStepdefs.java
@@ -0,0 +1,24 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class MyStepdefs {
+    @Given("^I have my game class$")
+    public void i_have_my_game_class() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I create new game$")
+    public void i_create_new_game() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I have created field with size (\\d+)x(\\d+) and numbers from (\\d+) to (\\d+) and one empty cell$")
+    public void i_have_created_field_with_size_x_and_numbers_from_to_and_one_empty_cell(int arg1, int arg2, int arg3, int arg4) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
